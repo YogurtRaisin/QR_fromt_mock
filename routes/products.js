821d3dac20a53fc624fc9dbf76874fb046280cdf
Kenji Kobayashi <is0355ks@ed.ritsumei.ac.jp>
2019-11-24 13:26:03 +0900

    feat: mock-up front server

--- routes/products.js
+++ routes/products.js
@@ -1,13 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
+// controllerが使うべき
+const rp = require('request-promise');
+// ここまで
+
+// 動的生成するべきだけどとりあえず配列
+const products = ['sample', 'hoge', 'fuga'];
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('select pay page');
+  res.render('products', { title: 'sample_pay', products: products});
 });
 
-router.get('/sample', function(req, res, next) {
-  res.render('pay', { title: 'sample_pay', product_name: 'sample' });
+router.get('/:productName', function(req, res, next) {
+  // controllerがすべき処理
+  let apiRes, isPayCode, payErr;
+  (async () => {
+    try{
+      apiRes = await rp('http://localhost:5000/api');
+      isPayCode = true;
+      payErr = null;
+      console.log(apiRes);
+      res.render('pay', { title: 'pay', productName: req.params.productName, payCode: apiRes.payCode, isPayCode: isPayCode, payErr:payErr});
+    } catch(err) {
+      apiRes = null;
+      isPayCode = false;
+      payErr = err.message;
+      console.error(err);
+      res.render('pay', { title: 'pay', productName: req.params.productName, payCode: apiRes.payCode, isPayCode: isPayCode, payErr:payErr});
+    }
+  })().catch(next);
+  // ここまで
+
 });
 
 module.exports = router;
